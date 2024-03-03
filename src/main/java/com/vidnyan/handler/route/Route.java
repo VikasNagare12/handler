e7cbcc518db115d2675dc561dc1e63e2f89d19da
Vikas Nagare <vikasnagare05@gmail.com>
2024-03-03 19:23:43 +0530

    Initial commit

---
+++ src/main/java/com/vidnyan/handler/route/Route.java
@@ -0,0 +1,19 @@
+package com.vidnyan.handler.route;
+
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Route {
+
+    @Bean
+    public void routes(){
+
+    }
+
+
+
+
+}
